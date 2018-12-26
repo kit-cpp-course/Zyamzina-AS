583a1b21ebf616e3b89bf1ff8426a6b552076762
anastaiyazyamzina <45993670+anastaiyazyamzina@users.noreply.github.com>
2018-12-26 14:48:41 +0300

    Update FileTypesRepo.h

--- FileTypesRepo.h
+++ FileTypesRepo.h
@@ -4,16 +4,28 @@
 #include <string>
 #include <map>
 #include <vector>
+#include "Configuration.h"
 
 namespace catalogize {
+	/*
+		Репозиторий типов файлов. Отвечает за определение категории файла по расширению.
+	*/
 	class FileTypesRepo
 	{
 	private:
 		std::map<std::wstring, std::wstring> extensionToType;
 	public:
-		FileTypesRepo();
-		std::wstring* getCategory(std::wstring* extension);
-		~FileTypesRepo();
+		FileTypesRepo() {};
+		FileTypesRepo(FileTypesRepo const&) = delete;
+		void operator=(FileTypesRepo const&) = delete;
+		static FileTypesRepo& instance() {
+			static FileTypesRepo m_instance;
+			return m_instance;
+		};
+		// Инициализирует репозиторий типов файлов из файла, указанного в конфигурации.
+		int load();
+		// Возвращает имя категории по расширению файла
+		std::wstring & getCategory(std::wstring & extension);
 	};
 
 }
