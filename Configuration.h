94e74ceef54fe8d24afc292a197c13125cca83c6
anastaiyazyamzina <45993670+anastaiyazyamzina@users.noreply.github.com>
2018-12-26 14:56:55 +0300

    Create Configuration.h

---
+++ Configuration.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <fstream>
+#include <string>
+#include "Constants.h"
+#include "filesystem.h"
+
+using namespace std;
+namespace catalogize {
+	class ArgParser;
+	/*
+		Конфигурация приложения.
+	*/
+	class Configuration
+	{
+		friend ArgParser;
+		wstring* m_from;
+		wstring* m_to;
+		wstring* m_repo_path;
+	public:
+		Configuration() {};
+		static Configuration& instance() {
+			static Configuration m_instance;
+			return m_instance;
+		};
+		Configuration(Configuration const&) = delete;
+		void operator=(Configuration const&) = delete;
+
+		wstring& from() const { return *m_from; };
+		wstring& to() const { return *m_to; };
+		wstring& repo() {
+			if (m_repo_path == nullptr) {
+				m_repo_path = new wstring(L".\\types.repo");
+			}
+			return *m_repo_path;
+		};
+		// Проверяет конфигурацию на наличие ошибок
+		ConfigurationState verify();
+
+		~Configuration();
+	};
+}
+
