67633faa414bf47867617619e1ca9247c5b30e71
anastaiyazyamzina <45993670+anastaiyazyamzina@users.noreply.github.com>
2018-12-26 14:48:01 +0300

    Update FileTypesRepo.cpp

--- FileTypesRepo.cpp
+++ FileTypesRepo.cpp
@@ -1,24 +1,31 @@
-﻿#include "FileTypesRepo.h"
-#include "TypeMappings.h"
+#include "FileTypesRepo.h"
 
 using namespace std;
 
 namespace catalogize {
-	
-	FileTypesRepo::FileTypesRepo()
+
+	int FileTypesRepo::load()
 	{
-		for (int i = (sizeof(TYPE_NAMES) / sizeof(TYPE_NAMES[0])) - 1; i >= 0; i--) {
-			for (int j = TYPE_LENGTHS[i] - 1; j >= 0; j--) {
-				extensionToType[TYPES[i][j]] = TYPE_NAMES[i];
-			}
+		Configuration &conf = Configuration::instance();
+		wifstream input;
+		input.open(conf.repo());
+
+		int loaded = 0;
+		wstring line, extension, category;
+		while (input.good()) {
+			getline(input, line);
+			int separator = line.find(':');
+			extension = line.substr(0, separator);
+			category = line.substr(separator + 1, line.length() - separator);
+			extensionToType[extension] = category;
+			loaded++;
 		}
+		return loaded;
 	}
-	
+
 	/* возвращает расширения(типа) файла */
-	std::wstring* FileTypesRepo::getCategory(std::wstring* extension)
+	std::wstring& FileTypesRepo::getCategory(std::wstring& extension)
 	{
-		return &extensionToType[*extension];
+		return extensionToType[extension];
 	}
-	
-	FileTypesRepo::~FileTypesRepo(){}
 }
